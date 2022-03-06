0833232b2c17e2259eaba999dd0bd902e2e83240
Nasirdin <zhaparkulov02@gmail.com>
2022-03-06 13:37:10 +0600

    millor

---
+++ src/components/home/section2/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.scss';
+
+const Section2 = () => {
+    return (
+        <div className='section2' id='section2'>
+            <div className='container'>
+                <h2 className='section2__title title'>Каталоги нашей продукции</h2>    
+            </div>
+        </div>
+    )
+}
+
+export default Section2;
