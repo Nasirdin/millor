d7934c265ea2e3469cd3f0261f1a70042d7961ae
Nasirdin <zhaparkulov02@gmail.com>
2022-03-07 14:58:06 +0600

    add section catalog

---
+++ src/components/home/section3/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+import './style.scss';
+
+const Section3 = () => {
+    return (
+        <div className='section3' id='section3'>
+            <div className='container'>
+                <h2 className='section3__title title'>Товары со скидкой</h2>
+                <p className='section3__subtitle subtitle'>Наша компания предлагает покупать товар со скидкой не только в дни распродаж или в течение действия ограниченных предложений, но и пользоваться скидками постоянно!</p>
+            </div>
+        </div>
+    )
+}
+
+export default Section3;
